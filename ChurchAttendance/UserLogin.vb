04bb99cb7524a044902d12fef7e4a626b05f2495
user_name <email_id>
2021-02-02 09:47:02 +0000

    this too

--- ChurchAttendance/UserLogin.vb
+++ ChurchAttendance/UserLogin.vb
@@ -17,7 +17,7 @@
     End Sub
 
     Private Sub txtUsername_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtUsername.KeyPress
-        if Not (Asc(e.KeyChar) = 8) Then
+        if Not (Asc(e.KeyChar) = 8) And Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
             if Not (Asc(e.KeyChar) >= 97 And Asc(e.KeyChar) <= 122) Or (Asc(e.KeyChar) >= 65 And Asc(e.KeyChar)<90) Then
                 e.KeyChar = ChrW(0)
                 e.Handled = True
