acc5939c5c27645fa7b66af4a5daa81f9c599e48
user_name <email_id>
2021-02-02 00:34:58 +0000

    Amended

--- ChurchAttendance/UserForm.vb
+++ ChurchAttendance/UserForm.vb
@@ -1,3 +1,22 @@
 ﻿Public Class UserForm
+    Private Sub txtUsername_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtUsername.KeyPress
+            if Not (Asc(e.KeyChar) = 8) Then
+                if Not (Asc(e.KeyChar) >= 97 And Asc(e.KeyChar) <= 122) Or (Asc(e.KeyChar) >= 65 And Asc(e.KeyChar)<90) Then
+                    e.KeyChar = ChrW(0)
+                    e.Handles = True
+                End if
+            End If
+        
+    End Sub
+
+    Private Sub txtMemberID_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtMemberID.KeyPress
+            if Not (Asc(e.KeyChar) = 8) Then
+                if Not (Asc(e.KeyChar) >= 97 And Asc(e.KeyChar) <= 122) Or (Asc(e.KeyChar) >= 65 And Asc(e.KeyChar)<90) Then
+                    e.KeyChar = ChrW(0)
+                    e.Handles = True
+                End if
+            End If
+        
+    End Sub
 
 End Class
