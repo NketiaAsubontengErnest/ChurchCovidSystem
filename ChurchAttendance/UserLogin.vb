530358e1f576f73fb78024a9a375a538038552d9
user_name <email_id>
2021-02-02 19:13:42 +0000

    out

--- ChurchAttendance/UserLogin.vb
+++ ChurchAttendance/UserLogin.vb
@@ -20,41 +20,4 @@
         ResetPassword.Show()
         Me.Hide()
     End Sub
-
-    Private Sub txtUsername_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtUsername.KeyPress
-        if Not (Asc(e.KeyChar) = 8) And Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
-            if Not (Asc(e.KeyChar) >= 97 And Asc(e.KeyChar) <= 122) Or (Asc(e.KeyChar) >= 65 And Asc(e.KeyChar)<90) Then
-                e.KeyChar = ChrW(0)
-                e.Handled = True
-            End if
-        End If
-       
-    End Sub
-
-    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
-        If txtUsername.Text = "" Then
-            MsgBox("Username field CANNOT be empty", MsgBoxStyle.Critical, "Warning")
-            txtUsername.Clear()
-            txtUsername.Focus()
-        ElseIf txtPassword.Text = "" Then
-            MsgBox("Password field CANNOT be empty", MsgBoxStyle.Critical, "Warning")
-            txtPassword.Clear()
-            txtPassword.Focus()
-        ElseIf txtUsername.Text = "admin" And txtPassword.text = "00000" Then
-            AdminDashboard.Show()
-
-        ElseIf txtUsername.Text = "user" And txtPassword.text = "11111" Then
-            UserDashboard.Show()
-
-        Else
-            MsgBox("Wrong Username or Password", MsgBoxStyle.Critical, "WARNING")
-            txtUsername.Clear()
-            txtPassword.Clear()
-
-            txtUsername.Focus()
-            txtPassword.Focus()
-
-        End If
-        Me.Hide()
-    End Sub
 End Class
