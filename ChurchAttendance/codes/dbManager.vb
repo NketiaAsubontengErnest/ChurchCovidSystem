26d835acaeeb78d35aa3b8656bd7df897854a8a4
Digisoft23 <digisoft.inc19@gmail.com>
2021-02-02 08:35:56 +0800

    Update

---
+++ ChurchAttendance/codes/dbManager.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data.SqlClient
+
+''' <summary>
+''' This class is used to make database connections and various querying of the database
+''' </summary>
+Public Class dbManager
+    Private myConn As SqlConnection
+    Private cmd As SqlCommand
+    Private reader As SqlDataReader
+
+
+
+
+End Class
