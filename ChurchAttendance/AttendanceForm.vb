a096b336b195f8e5b5f258c6d7eee16f4f776971
user_name <email_id>
2021-02-02 02:12:43 +0000

    hunged

--- ChurchAttendance/AttendanceForm.vb
+++ ChurchAttendance/AttendanceForm.vb
@@ -1,3 +1,13 @@
 ﻿Public Class AttendanceForm
+    Private Sub txtMembershipID_KeyPress(sender As Object, e As KeyPressEventArgs) Handled txtMembershipID.KeyPress
+        
+    End Sub
 
+    Private Sub txtTemperature_KeyPress(sender As Object, e As KeyPressEventArgs) Handled txtTemperature.KeyPress
+        
+    End Sub
+
+    Private Sub txtServiceId_KeyPress(sender As Object, e As KeyPressEventArgs) Handled txtServiceId.KeyPress
+        
+    End Sub
 End Class
